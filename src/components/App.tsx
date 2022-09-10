9dfef94eaa6f7a04f3be778a3308a94c813d0e73
Dwight Smith <dwightsmth@gmail.com>
2022-09-10 16:23:58 -0400

    mobile viewport auth page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import TopNav from './navigation/TopNav'
 import BottomNav from './navigation/BottomNav'
 import { Search } from './navigation/Search'
+import { ErrorBlock } from 'antd-mobile'
 
+const viewportQuery = window.matchMedia('(max-width: 448px)')
 function App() {
+  const [isMobile, setIsMobile] = useState<boolean>(viewportQuery.matches)
+
+  useEffect(() => {
+    viewportQuery.onchange = () => setIsMobile(viewportQuery.matches)
+  }, [])
+
+  if(!isMobile) return <ErrorBlock fullPage status='disconnected' title='Mobile Only!' description='Please use mobile viewport...'/>
+
   return (
     <section className='app'>
       <TopNav/>
       <Search/>
-      <Outlet/>
+      <div className='outlet-container'>
+        <Outlet/>
+      </div>
       <BottomNav/>
     </section>
   )
